145c90b1df37e103705075faa59591544aba837f
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-29 12:37:06 -0400

    changed fsa website link

--- Portfolio/portfolio/pages/fsatravel.js
+++ Portfolio/portfolio/pages/fsatravel.js
@@ -81,7 +81,7 @@ const fsatravel = () => {
             <button className='px-8 py-2 mt-4 mr-8'>Code</button>
           </a>
           <a
-            href='https://fsa-travel-glub.onrender.com/home'
+            href='https://fsa-travel-glub.onrender.com'
             target='_blank'
             rel='noreferrer'
           >
