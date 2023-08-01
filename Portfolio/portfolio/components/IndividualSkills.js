fc4b8017c0af78f0760524922aa9ecd9c301c78f
Michael Kleyman <michaelkleyman99@gmail.com>
2023-08-01 17:15:18 -0400

    added AWS logo to skills

--- Portfolio/portfolio/components/IndividualSkills.js
+++ Portfolio/portfolio/components/IndividualSkills.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import Image from 'next/image';
-import Javascriptimg from '../public/images/javascript.png';
-import Typescriptimg from '../public/images/typescript.png';
-import Pythontimg from '../public/images/python.png';
-import Htmlimg from '../public/images/html.png';
-import Cssimg from '../public/images/css.png';
-import Reactjsimg from '../public/images/reactjs.png';
-import Nextjsimg from '../public/images/nextjs.png';
-import Firebaseimg from '../public/images/firebase.png';
-import Tailwindimg from '../public/images/tailwind.png';
-import Reduximg from '../public/images/redux.png';
-import Postgresimg from '../public/images/postgres.png';
-import Sequelizeimg from '../public/images/sequelize.png';
-import Nodeimg from '../public/images/node.png';
+import React from "react";
+import Image from "next/image";
+import Javascriptimg from "../public/images/javascript.png";
+import Typescriptimg from "../public/images/typescript.png";
+import Pythontimg from "../public/images/python.png";
+import Htmlimg from "../public/images/html.png";
+import Cssimg from "../public/images/css.png";
+import Reactjsimg from "../public/images/reactjs.png";
+import Nextjsimg from "../public/images/nextjs.png";
+import Firebaseimg from "../public/images/firebase.png";
+import Tailwindimg from "../public/images/tailwind.png";
+import Reduximg from "../public/images/redux.png";
+import Postgresimg from "../public/images/postgres.png";
+import Sequelizeimg from "../public/images/sequelize.png";
+import Nodeimg from "../public/images/node.png";
+import AWSimg from "../public/images/aws.png";
 
 const IndividualSkills = () => {
   const skills = [
-    { img: Typescriptimg, name: 'Typescript' },
-    { img: Javascriptimg, name: 'javascript' },
-    { img: Pythontimg, name: 'python' },
-    { img: Htmlimg, name: 'html' },
-    { img: Cssimg, name: 'css' },
-    { img: Reactjsimg, name: 'reactJS' },
-    { img: Nextjsimg, name: 'nextJS' },
-    { img: Firebaseimg, name: 'firebase' },
-    { img: Tailwindimg, name: 'tailwind' },
-    { img: Reduximg, name: 'redux' },
-    { img: Postgresimg, name: 'postgres' },
-    { img: Sequelizeimg, name: 'sequelize' },
-    { img: Nodeimg, name: 'node' },
+    { img: Typescriptimg, name: "Typescript" },
+    { img: Javascriptimg, name: "javascript" },
+    { img: AWSimg, name: "AWS" },
+    { img: Pythontimg, name: "python" },
+    { img: Htmlimg, name: "html" },
+    { img: Cssimg, name: "css" },
+    { img: Reactjsimg, name: "reactJS" },
+    { img: Nextjsimg, name: "nextJS" },
+    { img: Firebaseimg, name: "firebase" },
+    { img: Tailwindimg, name: "tailwind" },
+    { img: Reduximg, name: "redux" },
+    { img: Postgresimg, name: "postgres" },
+    { img: Sequelizeimg, name: "sequelize" },
+    { img: Nodeimg, name: "node" },
   ];
 
   return (
@@ -36,7 +38,7 @@ const IndividualSkills = () => {
       {skills.map((skill, i) => (
         <div
           key={i}
-          className='p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300'
+          className='p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 flex items-center justify-center'
         >
           <div className='grid grid-cols-2 gap-4 justify-center items-center'>
             <div className='m-auto'>
